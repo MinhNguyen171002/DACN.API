d4e1cbf2d15fd767e9ea00180f25bb0ab757282a
MinhNguyen171002 <minhnguyen03092002@gmail.com>
2024-03-13 00:05:50 +0700

    code 1 setup CRUD

--- API/Repositories/URepositories.cs
+++ API/Repositories/URepositories.cs
@@ -5,16 +5,56 @@ using System.Linq.Expressions;
 
 namespace API.Repositories
 {
-/*    public class IURepositories : IRepository<Users>
+    public interface IURepositories : IRepository<User>
     {
-
+        public void insertUser(User user);
     }
-    public class UserRepository : RepositoryBase<Users>, IURepositories
+    public class URepository : RepositoryBase<User>, IURepositories
     {
-        public UserRepository(DB dbContext) : base(dbContext)
+        public URepository(DB dbContext) : base(dbContext)
         {
         }
+        public void insertUser(User user)
+        {
+            _dbContext.users.Add(user);
+        }
+        public void insertLevel(Level level)
+        {
+            _dbContext.levels.Add(level);
+        }
+        public void insertExam(Exam exam)
+        {
+            _dbContext.exams.Add(exam);
+        }
+        public void insertQuestion(Question question)
+        {
+            _dbContext.questions.Add(question);
+        }
+        public void insertResult(Result result)
+        {
+            _dbContext.results.Add(result);
+        }
+        public void DeleteUser(User user)
+        {
+            _dbContext.users.Remove(user);
+        }
+        public void DeleteLevel(Level level)
+        {
+            _dbContext.levels.Remove(level);
+        }
+        public void DeleteExam(Exam exam)
+        {
+            _dbContext.exams.Remove(exam);
+        }
+        public void DeletetQuestion(Question question)
+        {
+            _dbContext.questions.Remove(question);
+        }
+        public void DeleteResult(Result result)
+        {
+            _dbContext.results.Remove(result);
+        }
 
-    }*/
+    }
 }
     
