932a9c92d71b23b18916d6af9ced01e5b05ee228
MinhNguyen171002 <minhnguyen03092002@gmail.com>
2024-03-13 20:04:52 +0700

    Setup CRUD

---
+++ API/Repositories/LevelRepositories.cs
@@ -0,0 +1,31 @@
+﻿using API.Data;
+using API.DBContext;
+using API.Enity;
+
+namespace API.Repositories
+{
+    public interface ILevelRepositories : IRepository<Level>
+    {
+
+    }
+    public class LevelRepository : RepositoryBase<Level>, ILevelRepositories
+    {
+        public LevelRepository(DB dbContext) : base(dbContext)
+        {
+
+        }
+        public void insertLevel(Level level)
+        {
+            _dbContext.levels.Add(level);
+        }
+        public void deleteLevel(Level level)
+        {
+            _dbContext.levels.Remove(level);
+        }
+        public void updateLevel(Level level)
+        {
+            _dbContext.levels.Attach(level);
+            _dbContext.Entry(level).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+        }
+    }
+}
---
+++ API/Repositories/QuestionRepositories.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using API.DBContext;
+using API.Enity;
+
+namespace API.Repositories
+{
+    public interface IQuestionRepositories : IRepository<Question>
+    {
+
+    }
+    public class QuestionRepository : RepositoryBase<Question>, IQuestionRepositories
+    {
+        public QuestionRepository(DB dbContext) : base(dbContext)
+        {
+        }
+        public void insertQuestion(Question question)
+        {
+            _dbContext.questions.Add(question);
+        }
+        public void deletetQuestion(Question question)
+        {
+            _dbContext.questions.Remove(question);
+        }
+        public void updateQuestion(Question question)
+        {
+            _dbContext.questions.Attach(question);
+            _dbContext.Entry(question).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+        }
+    }
+}
