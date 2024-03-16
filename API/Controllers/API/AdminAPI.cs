db967d7ae23645df4830fdf1eb9822560ba97aba
MinhNguyen171002 <minhnguyen03092002@gmail.com>
2024-03-16 15:48:50 +0700

    16/3/2024 API dang ky, dang nhap, dang xuat

--- API/Controllers/API/AdminAPI.cs
+++ API/Controllers/API/AdminAPI.cs
@@ -1,6 +1,14 @@
 ﻿using API.DBContext;
+using API.Enity;
 using API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Model;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace API.Controllers.API
 {
@@ -14,8 +22,17 @@ namespace API.Controllers.API
         ExamService examService;
         QuestionService questionService;
         ResultService resultService;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IConfiguration _configuration;
+        private RoleManager<IdentityRole> _roleManager
+        {
+            get;
+        }
         public AdminAPI(DB dBContext, UserService userService, LevelService levelService, 
-            ExamService examService, QuestionService questionService, ResultService resultService)
+            ExamService examService, QuestionService questionService, ResultService resultService, 
+            RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> _userManager, 
+            SignInManager<IdentityUser> _signInManager, IConfiguration _configuration)
         {
             this.dBContext = dBContext;
             this.userService = userService;
@@ -23,8 +40,73 @@ namespace API.Controllers.API
             this.examService = examService;
             this.questionService = questionService;
             this.resultService = resultService;
+            this._roleManager = roleManager;
+            this._userManager = _userManager;
+            this._signInManager = _signInManager;
+            this._configuration = _configuration;
         }
         [HttpPost]
-        [Route("add")]
+        [AllowAnonymous]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] Register model)
+        {
+            var userExists = await _userManager.FindByNameAsync(model.UserName);
+            if (userExists != null)
+                return StatusCode(StatusCodes.Status500InternalServerError, new { Status = "Error", Message = "User already exists!" });
+
+            IdentityUser user = new IdentityUser()
+            {
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.UserName                
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+                return StatusCode(StatusCodes.Status500InternalServerError, new { Status = "Error", Message = "User creation failed! Please check user details and try again." });
+
+            User user1 = new User()
+            {
+                UserID = user.Id,
+                UserName = model.UserName,
+                Email = model.Email,
+                SDT = model.SDT
+            };
+            userService.insert(user1);
+            return Ok(new Response { Status = true, Message = "Welcome" });
+        }
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> SignIn([FromBody] LoginModel login)
+        {
+            var user = await _userManager.FindByNameAsync(login.Username);
+            if (user != null && await _userManager.CheckPasswordAsync(user, login.Password) && (await _signInManager.PasswordSignInAsync(user, login.Password, false, false)).Succeeded)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim("id", user.Id)
+                };
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, userRole));
+                }
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+                var token = new JwtSecurityToken(
+                    expires: DateTime.UtcNow.AddSeconds(300),
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+                return Ok(new LoginResponse
+                {
+                    Token = new JwtSecurityTokenHandler().WriteToken(token),
+                    Status = true
+                });
+            }
+            return Unauthorized();
+        }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,13 @@
 using API.DBContext;
+using API.Repositories;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Pomelo.EntityFrameworkCore.MySql;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,12 +24,45 @@ var con = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<DB>(options =>
     options.UseMySql(con,ServerVersion.AutoDetect(con))
 );
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<DB>()
+                .AddDefaultTokenProviders();
+builder.Services.AddScoped<IUserRepositories, UserRepository>();
+builder.Services.AddScoped<ILevelRepositories, LevelRepository>();
+builder.Services.AddScoped<IExamRepositories, ExamRepository>();
+builder.Services.AddScoped<IQuestionRepositories, QuestionRepository>();
+builder.Services.AddScoped<IResultRepositories, ResultRepository>();
+
+builder.Services.AddTransient<UserService>();
+builder.Services.AddTransient<LevelService>();
+builder.Services.AddTransient<ExamService>();
+builder.Services.AddTransient<QuestionService>();
+builder.Services.AddTransient<ResultService>();
 
 builder.Services.AddAuthorization(options =>
 {
     // By default, all incoming requests will be authorized according to the default policy.
     options.FallbackPolicy = options.DefaultPolicy;
 });
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+                };
+            });
 
 var app = builder.Build();
 
