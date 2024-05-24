14bd21abd748ba932bb959359db3a351a14325fb
MinhNguyen171002 <minhnguyen03092002@gmail.com>
2024-05-24 20:24:33 +0700

    24/04/2024

--- API/Model/DTO/ExamDTO.cs
+++ API/Model/DTO/ExamDTO.cs
@@ -2,8 +2,8 @@
 {
     public class ExamDTO
     { 
-        public string UserID { get; set; }
-        public int ExamID { get; set; }
+        public string? UserID { get; set; }
+        public int? ExamID { get; set; }
         public string? ExamDescription { get; set; }
         public string? Skill { get; set; }
         public TimeSpan ExamDuration { get; set; }
--- API/Model/DTO/QuestionComDTO.cs
+++ API/Model/DTO/QuestionComDTO.cs
@@ -2,9 +2,9 @@
 {
     public class QuestionComDTO
     {
-        public string UserID { get; set; }
-        public string QuestionID { get; set; }
-        public int QuestionSerial { get; set; }
+        public string? UserID { get; set; }
+        public string? QuestionID { get; set; }
+        public int? QuestionSerial { get; set; }
         public string? QuestionChoose { get; set; }
         public bool? IsCorrect { get; set; }
         public string? SentenceID { get; set; }
